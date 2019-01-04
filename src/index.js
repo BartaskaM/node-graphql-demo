6be01b7b7d80dfaeb6278f7a45d7b392adbd844c
Matas Bartaska <matas.bartaska@devbridge.com>
2019-01-04 13:38:47 +0200

    Add vote

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
 
 const resolvers = {
     Query,
     Mutation,
     User,
     Link,
+    Vote,
 };
 
 const server = new GraphQLServer({
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const feed = (root, args, context) => context.prisma.links();
 const link = (parent, args, context) => context.prisma.link({id: args.id});
+const votes = (parent, args, context) => context.prisma.votes();
 
 module.exports = {
     feed,
     link,
+    votes,
 };
